58695a63b3929f1683e17a2198caa242de56edd1
ZhekLu <justletmego@list.ru>
2022-05-24 15:22:15 +0300

    Add login/logout

--- paintsite/urls.py
+++ paintsite/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from paintsite.views import index, other_page
+from paintsite.views import index, other_page, PSLoginView, profile, PSLogoutView
 
 app_name = 'paintsite'
 
 urlpatterns = [
+    path('accounts/logout/', PSLogoutView.as_view(), name='logout'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/login/', PSLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
 ]
--- paintsite/views.py
+++ paintsite/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
-
+from django.contrib.auth.views import LoginView, LogoutView
 
 def index(request):
     return render(request, 'paintsite/index.html')
@@ -15,3 +17,15 @@ def other_page(request, page):
         raise Http404
     return HttpResponse(template.render(request=request))
 
+
+@login_required
+def profile(request):
+    return render(request, 'paintsite/profile.html')
+
+
+class PSLoginView(LoginView):
+    template_name = 'paintsite/login.html'
+
+
+class PSLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'paintsite/logout.html'
