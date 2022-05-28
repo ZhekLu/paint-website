ced77ec9907ff744e8aa47ec1fdc7b99bf90d6e0
ZhekLu <justletmego@list.ru>
2022-05-28 14:18:25 +0300

    Service functional

--- ppclient/src/app/pp.service.ts
+++ ppclient/src/app/pp.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {catchError, Observable, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PpService {
+  private url: String = 'http://localhost:8000';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getPps(): Observable<Object[]> {
+    return this.http.get<Object[]>(this.url + '/api/pps/');
+  }
+
+  getPp(pk: Number): Observable<Object> {
+    return this.http.get<Object>(this.url + '/api/pps/' + pk);
+  }
+
+  handleError() {
+    return (error: any): Observable<Object> => {
+      window.alert(error.message);
+      return of(0);
+    }
+  }
+
+  addComment(pp: String, author: String, password: String, content: String)
+  : Observable<Object> {
+    const comment = {'pp': pp, 'author': author, 'content': content};
+    const options = {headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic ' + window.btoa(author + ':' + password)
+    })};
+    return this.http.post<Object>(this.url + '/api/pps/' + pp + '/comments/',
+      comment, options).pipe(catchError(this.handleError()));
+  }
+
+  getComments(pk: Number): Observable<Object[]> {
+    return this.http.get<Object[]>(this.url + '/api/pps/' + pk + '/comments/');
+  }
 }
