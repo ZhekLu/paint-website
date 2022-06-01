df1d04d3850abf49fa370a27ef889838b8632c24
ZhekLu <justletmego@list.ru>
2022-06-01 11:15:47 +0300

    paintapp : parametrized tests

--- paintapp/test/test_views.py
+++ paintapp/test/test_views.py
@@ -1,47 +1,29 @@
 from django.test import TestCase
 from django.urls import reverse
+from parameterized import parameterized
 
 
-class NewPaintViewTest(TestCase):
-    def test_view_url_exists_at_desired_location(self):
-        resp = self.client.get('/paintapp/')
-        self.assertEqual(resp.status_code, 200)
+class TestViewsSequence(TestCase):
+    @parameterized.expand([
+        ['paint', '/paintapp/', 'paintapp:new_paint', 'paintapp/new_paint.html'],
+        ['old', '/paintapp/old/', 'paintapp:old_paint', 'paintapp/old_paint.html'],
+        ['info', '/paintapp/info/', 'paintapp:info', 'paintapp/info.html'],
+    ])
+    def test_sequence(self, name, url_path, reverse_path, template_path):
+        def test_view_url_exists_at_desired_location():
+            resp = self.client.get(url_path)
+            self.assertEqual(resp.status_code, 200)
 
-    def test_view_url_accessible_by_name(self):
-        resp = self.client.get(reverse('paintapp:new_paint'))
-        self.assertEqual(resp.status_code, 200)
+        def test_view_url_accessible_by_name():
+            resp = self.client.get(reverse(reverse_path))
+            self.assertEqual(resp.status_code, 200)
 
-    def test_view_uses_correct_template(self):
-        resp = self.client.get(reverse('paintapp:new_paint'))
-        self.assertEqual(resp.status_code, 200)
-        self.assertTemplateUsed(resp, 'paintapp/new_paint.html')
+        def test_view_uses_correct_template():
+            resp = self.client.get(reverse(reverse_path))
+            self.assertEqual(resp.status_code, 200)
+            self.assertTemplateUsed(resp, template_path)
 
+        test_view_url_exists_at_desired_location()
+        test_view_url_accessible_by_name()
+        test_view_uses_correct_template()
 
-class OldPaintViewTest(TestCase):
-    def test_view_url_exists_at_desired_location(self):
-        resp = self.client.get('/paintapp/old/')
-        self.assertEqual(resp.status_code, 200)
-
-    def test_view_url_accessible_by_name(self):
-        resp = self.client.get(reverse('paintapp:old_paint'))
-        self.assertEqual(resp.status_code, 200)
-
-    def test_view_uses_correct_template(self):
-        resp = self.client.get(reverse('paintapp:old_paint'))
-        self.assertEqual(resp.status_code, 200)
-        self.assertTemplateUsed(resp, 'paintapp/old_paint.html')
-
-
-class InfoViewTest(TestCase):
-    def test_view_url_exists_at_desired_location(self):
-        resp = self.client.get('/paintapp/info/')
-        self.assertEqual(resp.status_code, 200)
-
-    def test_view_url_accessible_by_name(self):
-        resp = self.client.get(reverse('paintapp:info'))
-        self.assertEqual(resp.status_code, 200)
-
-    def test_view_uses_correct_template(self):
-        resp = self.client.get(reverse('paintapp:info'))
-        self.assertEqual(resp.status_code, 200)
-        self.assertTemplateUsed(resp, 'paintapp/info.html')
