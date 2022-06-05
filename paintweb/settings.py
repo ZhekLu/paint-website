dc34b70a991cbcf256b359f8e9d384171302ba29
ZhekLu <justletmego@list.ru>
2022-06-05 09:13:10 +0300

    Dumped data

--- paintweb/settings.py
+++ paintweb/settings.py
@@ -74,10 +74,6 @@ WSGI_APPLICATION = 'paintweb.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'paintweb_db',
