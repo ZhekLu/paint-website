4a2247815561d877e8491f5f401b8f201e0212e1
ZhekLu <justletmego@list.ru>
2022-06-10 10:27:01 +0300

    Collect :)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-RUN echo "Collect static..."
-RUN python manage.py collectstatic --noinput
-RUN echo "Done."
+echo "Collect static..."
+python manage.py collectstatic --noinput
+echo "Done."
 
 #python manage.py flush --no-input
 python manage.py makemigrations --no-input
