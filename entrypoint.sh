aa1fae0d10a50d27a030929cdebfad3ed7ff82de
ZhekLu <justletmego@list.ru>
2022-06-10 13:32:55 +0300

    back + del run

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Collect static..."
 python manage.py collectstatic --noinput
@@ -8,7 +8,7 @@ echo "Done."
 python manage.py makemigrations --no-input
 python manage.py migrate
 
-python manage.py runserver 0.0.0.0:$PORT
+#python manage.py runserver 0.0.0.0:$PORT
 
 exec "$@"
 
