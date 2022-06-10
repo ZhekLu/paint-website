c59735f0144c629dbb2a7d69b4bb0cfee4fac8ff
ZhekLu <justletmego@list.ru>
2022-06-10 18:09:26 +0300

    Create superuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ echo "Collect static..."
 python manage.py collectstatic --noinput
 echo "Done."
 
+python manage.py createsuperuser --noinput
+
 #python manage.py flush --no-input
 python manage.py makemigrations --no-input
 python manage.py migrate
