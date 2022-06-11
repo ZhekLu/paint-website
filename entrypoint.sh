50318bd352b481e0b8a78e7357f850351b804c5b
ZhekLu <justletmego@list.ru>
2022-06-11 10:18:28 +0300

    Docker-compose.prod

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ echo "Collect static..."
 python manage.py collectstatic --noinput
 echo "Done."
 
-python manage.py createsuperuser --noinput
 
 #python manage.py flush --no-input
 python manage.py makemigrations --no-input
