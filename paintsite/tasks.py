30ecc1212e9d2d246e03913f92d7690dfc61e834
ZhekLu <justletmego@list.ru>
2022-06-14 22:52:29 +0300

    comment notif

--- paintsite/tasks.py
+++ paintsite/tasks.py
@@ -1,11 +1,13 @@
 from celery import shared_task
+# from celery.task import task
 from django.conf import settings
 from django.core.mail import send_mail
 from .models import Comment, User
 import datetime
+from paintweb.celery import app
 
 
-@shared_task
+@shared_task(bind=True)
 def comments_notification():
     date_from = datetime.datetime.now() - datetime.timedelta(days=1)
     comments = Comment.objects.filter(created_at__gte=date_from)
@@ -31,4 +33,23 @@ def comments_notification():
             recipient_list=[to_email],
             fail_silently=True,
         )
+    return True
+
+
+@app.task(bind=True, ignore_result=True, name='paintsite.tasks.send_comment_notification',
+          default_retry_delay=120, max_retries=5,
+          soft_time_limit=120, time_limit=125)
+def send_comment_notification(user: User, comment: Comment):
+    print("Here we are")
+    mail_subject = "You have new comment."
+    to_email = user.email
+    message = 'You received comments from ' + comment.author
+
+    send_mail(
+        subject=mail_subject,
+        message=message,
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[to_email],
+        fail_silently=True,
+    )
     return True
